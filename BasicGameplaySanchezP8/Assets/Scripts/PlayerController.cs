ca6a2aac0459e5762db80245b353f45051b4b148
Adam Sanchez <722945@student.dusd.net>
2024-03-15 15:28:13 -0700

    added more script
    added more script

--- BasicGameplaySanchezP8/Assets/Scripts/PlayerController.cs
+++ BasicGameplaySanchezP8/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInputl;
+    public GameObject projectilePrefab;
+    public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInputl = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInputl * Time.deltaTime * speed);
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
